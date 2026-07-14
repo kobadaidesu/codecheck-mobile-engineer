ff6aeeccc425ed123ae2dde2e494d5191568df28
kobadaidesu <daigokobayashi04@gmail.com>
2026-07-14 10:17:55 +0900

    feat: add user search response models

---
+++ Feature/Models/UserSummary.swift
@@ -0,0 +1,41 @@
+//
+//  UserSummary.swift
+//  kadai
+//
+//  Created by Kobayashi Daigo on 2026/07/13.
+//
+
+import Foundation
+
+import Foundation
+
+struct UserSearchResponse: Decodable {
+    let items: [UserSummary]
+}
+
+//Decodable: JSON -> Swiftの方に変換できる
+struct UserSummary: Decodable, Identifiable {
+    let id: Int
+    let login: String
+    let avatarURL: URL
+    let htmlURL: URL
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case login
+        case avatarURL = "avatar_url"
+        case htmlURL = "html_url"
+    }
+}
+
+
+/*
+{
+  "id": 1,
+  "login": "octocat",
+  "avatar_url": "https://...",
+  "html_url": "https://github.com/octocat"
+}
+CodingKeyをやらないとavatar_urlをきれいに受け取れない
+ これがあるとJSONの"html_url" -> SwiftのhtmlURLという対応になる
+*/
